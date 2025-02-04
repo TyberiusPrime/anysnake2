c5563d097e06506ccb909e0dba2725421dab6b04
Tyberius Prime <tyberius_prime@coonabibba.de>
2025-02-04 16:41:16 +0100

    nix functions cleanup

--- src/nix/functions.nix
+++ src/nix/functions.nix
@@ -66,45 +66,45 @@
     umoci = pkgs.umoci;
   in
     pkgs.runCommand "${name}.oci" {} ''
-    set -o pipefail
-    shopt -s nullglob
-    ${umoci}/bin/umoci init --layout "${name}"
-    ${umoci}/bin/umoci new --image "${name}:latest"
-    mkdir tmp-oci-unpack
+      set -o pipefail
+      shopt -s nullglob
+      ${umoci}/bin/umoci init --layout "${name}"
+      ${umoci}/bin/umoci new --image "${name}:latest"
+      mkdir tmp-oci-unpack
 
-    mkdir fakeroot/etc -p
-    touch fakeroot/etc/resolv.conf
+      mkdir fakeroot/etc -p
+      touch fakeroot/etc/resolv.conf
 
-    # umoci tries to read /etc/resolv.conf, so let's give it one..
-    ${pkgs.bubblewrap}/bin/bwrap \
-           --proc /proc \
-            --dev /dev \
-           --bind /build /build \
-           --ro-bind /build/fakeroot/etc /etc \
-           --ro-bind /nix /nix \
-      ${umoci}/bin/umoci unpack --image "${name}:latest" tmp-oci-unpack --rootless
+      # umoci tries to read /etc/resolv.conf, so let's give it one..
+      ${pkgs.bubblewrap}/bin/bwrap \
+             --proc /proc \
+              --dev /dev \
+             --bind /build /build \
+             --ro-bind /build/fakeroot/etc /etc \
+             --ro-bind /nix /nix \
+        ${umoci}/bin/umoci unpack --image "${name}:latest" tmp-oci-unpack --rootless
 
-    echo "rsyncing symlink forest"
-    ${pkgs.rsync}/bin/rsync -arW ${symlink_image.derivation}/rootfs/ tmp-oci-unpack/rootfs/
-    chmod +w tmp-oci-unpack/rootfs -R # because we don't have write on the directories
+      echo "rsyncing symlink forest"
+      ${pkgs.rsync}/bin/rsync -arW ${symlink_image.derivation}/rootfs/ tmp-oci-unpack/rootfs/
+      chmod +w tmp-oci-unpack/rootfs -R # because we don't have write on the directories
 
-    echo "rsyncing necessary nix paths"
-    ${pkgs.rsync}/bin/rsync -arW --exclude=* --files-from=${
+      echo "rsyncing necessary nix paths"
+      ${pkgs.rsync}/bin/rsync -arW --exclude=* --files-from=${
         pkgs.writeClosure [symlink_image.script_file]
       } / tmp-oci-unpack/rootfs/
 
 
-    # check that we have a rootfs/bin/sh
-    if [ ! -e tmp-oci-unpack/rootfs/bin/sh ]; then
-      echo "No rootfs/bin/sh found"
-      exit 1
-    fi
-    chmod +w tmp-oci-unpack/rootfs -R # because we don't have write on the directories
+      # check that we have a rootfs/bin/sh
+      if [ ! -e tmp-oci-unpack/rootfs/bin/sh ]; then
+        echo "No rootfs/bin/sh found"
+        exit 1
+      fi
+      chmod +w tmp-oci-unpack/rootfs -R # because we don't have write on the directories
 
-    ${umoci}/bin/umoci repack --image "${name}:latest" tmp-oci-unpack
-    ${umoci}/bin/umoci config --image "${name}:latest"
-    # strip the first component
-    cd "${name}" && tar cf $out .
+      ${umoci}/bin/umoci repack --image "${name}:latest" tmp-oci-unpack
+      ${umoci}/bin/umoci config --image "${name}:latest"
+      # strip the first component
+      cd "${name}" && tar cf $out .
 
 
     '';
@@ -118,48 +118,5 @@
   #       inherit name script;
   #     };
   #   in
-  #     pkgs.runCommand name {} ''
-  #       set -o pipefail
-  #       shopt -s nullglob
-  #       mkdir -p $out/rootfs/
-
-  #       ${pkgs.rsync}/bin/rsync -arW ${symlink_image.derivation}/rootfs/ $out/rootfs/
-  #       chmod +w $out/rootfs -R # because we don't have write on the directories
-  #       #make sure we got everything from the nix store, right?
-  #       ${pkgs.rsync}/bin/rsync -arW --exclude=* --files-from=${
-  #         pkgs.writeClosure [symlink_image.script_file]
-  #       } / $out/rootfs/
-
-  #       rm $out/rootfs/${symlink_image.script_file}
-  #       chmod 755 $out/rootfs
-
-  #       # # singularity tries to read resolv.conf, hosts and user definitions
-  #       # # when converting the container
-  #       # # so let's fake them
-  #       mkdir $out/etc
-  #       mkdir $out/build
-  #       touch $out/etc/resolv.conf
-  #       touch $out/etc/hosts
-  #       echo "nixbld:x:1000:2000:imtseq:/home/installer:/bin/bash\n" >$out/etc/passwd
-  #       echo "xxx:x: 2000:\n" >$out/etc/group
-  #       echo ${pkgs.singularity}/bin/singularity
-  #       ${pkgs.coreutils}/bin/whoami
-
-  #       # # also consider NIX_REDIRECT and libredirect for this
-  #       # the bash binding is needed for singularity to find 'sh'
-  #       ${pkgs.bubblewrap}/bin/bwrap \
-  #          --proc /proc \
-  #          --dev /dev \
-  #         --bind $out/ $out/ \
-  #         --bind $out/build /build \
-  #         --ro-bind $out/etc /etc \
-  #         --ro-bind /nix /nix \
-  #         --ro-bind "${pkgs.bash}/bin" /usr/bin \
-  #         ${pkgs.singularity}/bin/singularity build  /build/${name}.sif $out/rootfs -v
-  #       mv $out/build/*.sif $out/
-  #       rm -rf $out/build
-  #       rm -rf $out/etc
-  #       # chmod +w $out/rootfs -R # because we don't have write on the directories
-  #       # rm -rf $out/rootfs
-  #     '';
+  # ...
 }
